c352e6ae263745513d58f6964b18e7f0a1fdc0de
Raushan <rathoreraushan1139@gmail.com>
2026-06-28 07:49:46 +0530

    Update Reset Password Url

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -35,10 +35,12 @@ const userSchema = new mongoose.Schema({
   location: {
     type: {
         type: String,
+        enum:["Point"],
         default: "Point"
     },
     coordinates: {
-        type: [Number]
+        type: [Number],
+        default:[0,0]
     }
   },
   phone: {
