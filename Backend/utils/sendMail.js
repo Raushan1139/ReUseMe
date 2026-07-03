699eb2c1d41f012027c3f2e95a28a0bd943e7beb
Raushan <rathoreraushan1139@gmail.com>
2026-07-03 12:13:36 +0530

    Checking smtp connection

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+/*const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -7,5 +7,31 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS
     }
 });
+
+module.exports = transporter;
+
+*/
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+(async () => {
+  try {
+    await transporter.verify();
+    console.log("✅ SMTP Ready");
+  } catch (err) {
+    console.error("❌ SMTP Verify Error");
+    console.error(err);
+  }
+})();
 
 module.exports = transporter;
